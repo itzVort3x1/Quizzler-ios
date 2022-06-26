c93059f4187d8cd5f8374b0f51a9f0691beb4918
itzVort3x1 <saikaustubh10@gmail.com>
2022-06-26 10:42:34 +0530

    linked UI to code

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,12 +10,28 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var falseButton: UIButton!
     
+    let quiz = [
+        "Four + Two is equal to Six.",
+        "Five - Three is greater than One",
+        "Three + Eight is less than Ten",
+        "Delhi is the capital of India"
+    ]
+    
+    var questionNumber = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        questionLabel.text = quiz[questionNumber]
     }
-
+    
+    func updateUI(){
+        questionLabel.text = quiz[questionNumber];
+    }
+    
     @IBAction func answerButtonPressed(_ sender: Any) {
+        questionNumber += 1;
+        updateUI();
     }
     
 }
